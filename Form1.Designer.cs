b8299539355afbae65bfb74b4f3f871e3ebeea2a
Hamza Çakmak <reelaktor@gmail.com>
2019-03-29 23:35:07 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ColumnHeader columnHeader1;
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "urun_id";
+            columnHeader1.Width = 66;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(39, 24);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Location = new System.Drawing.Point(39, 79);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(269, 154);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "urun_adi";
+            this.columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "fiyati";
+            this.columnHeader3.Width = 81;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(213, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "fiyat güncelle";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 425);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //combobox1 de yer alan stunların altında yer alan bilgileri listview1 de 
+            //id no adını ve birim fiyatını ekleyen program
+            string[] dizi = comboBox1.SelectedItem.ToString().Split(' ');
+            int id = Convert.ToInt16(dizi[0]);
+            string str = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='F:\Gorsel prog\WindowsFormsApplication3\WindowsFormsApplication3\ticari.mdf';Integrated Security=True";
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            SqlCommand cmd2 = new SqlCommand("select * from stok where cat_id=@x", con);
+            cmd2.Parameters.AddWithValue("@x", id);
+            listView1.Items.Clear();
+            SqlDataReader dr = cmd2.ExecuteReader();
+            while (dr.Read())
+            {
+                ListViewItem item = listView1.Items.Add(dr["stok_id"].ToString());
+                item.SubItems.Add(dr["stok_name"].ToString());
+                item.SubItems.Add(dr["brfiyat"].ToString());
+               
+            }
+            con.Close();           
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Form Load oldugunda category tablosundaki stunları combobox1 e ekleyen program
+            string str = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='F:\Gorsel prog\WindowsFormsApplication3\WindowsFormsApplication3\ticari.mdf';Integrated Security=True";
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            SqlCommand cmd1 = new SqlCommand("select * from category", con);
+            comboBox1.Items.Clear();
+            SqlDataReader dr = cmd1.ExecuteReader();
+            while (dr.Read())
+            {
+                comboBox1.Items.Add(dr[0].ToString() + " " + dr["cat_name"].ToString());
+            }
+            con.Close();
+            comboBox1.SelectedIndex = 0;         
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //buton clik oldugunda comboboxda secilen kategorideki ürünlerin fiyatını % 10 arttıran program
+            string[] dizi = comboBox1.SelectedItem.ToString().Split(' ');
+            int id = Convert.ToInt16(dizi[0]);
+            string str = @"Data Source=(LocalDB)\v11.0;AttachDbFilename='F:\Gorsel prog\WindowsFormsApplication3\WindowsFormsApplication3\ticari.mdf';Integrated Security=True";
+            SqlConnection con = new SqlConnection(str);
+            con.Open();
+            SqlCommand cmd2 = new SqlCommand("update stok set brfiyat= convert(int,1.1*brfiyat) where  cat_id=@x", con);
+            cmd2.Parameters.AddWithValue("@x", id);
+            listView1.Items.Clear();
+            int adet = cmd2.ExecuteNonQuery();
+            MessageBox.Show(adet.ToString() +" kadar kayıt güncellendi");
+            con.Close();
+        }
+    }
+}
